75c6162af060f4961b6f0387f9c977339ec5023a
PhillipGameDev <267723+PhillipGameDev@users.noreply.github.com>
2022-01-28 23:16:40 -0300

    crouch

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 7;
+    [SerializeField] private float crouchSpeed = 5;
     [SerializeField] private float jumpForce = 10;
     [SerializeField] private int extraJumpNumber = 1;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private Vector2 groundCheckSize;
     [SerializeField] private LayerMask groundLayer;
+
+    [Header("Colliders")]
+    [SerializeField] private Collider2D collBody;
+    [SerializeField] private Collider2D collCrouching;
+
     private Rigidbody2D rb2d;
+    private Animator anim;
     private bool jumpInput;
+    private bool crouchInput;
     private bool jumping;
     private int jumpCount;
     private bool grounded;
-    public bool IsGrounded { 
+    private bool crouching;
+
+    public bool IsGrounded 
+    { 
         get => grounded; 
         private set
         {
@@ -28,12 +39,28 @@ public class Player : MonoBehaviour
             }
         } 
     }
+
+    public bool Crouching 
+    { 
+        get => crouching;
+        private set 
+        {
+            if (value != crouching)
+            {
+                crouching = value;
+
+                collBody.gameObject.SetActive(!crouching);
+                collCrouching.gameObject.SetActive(crouching && grounded);
+            }
+        }
+    }
     
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -49,7 +76,7 @@ public class Player : MonoBehaviour
         float axisHorizontal = Input.GetAxisRaw("Horizontal");
         Move(new Vector2(axisHorizontal, 0));
 
-        if (Input.GetButtonDown("Jump") && (grounded || jumpCount <= extraJumpNumber))
+        if (Input.GetButtonDown("Jump") && ((grounded  || jumpCount <= extraJumpNumber) && !Crouching))
         {
             jumpInput = true;
             jumpCount++;
@@ -62,6 +89,24 @@ public class Player : MonoBehaviour
             if (rb2d.velocity.y > 0)
                 rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
         }
+
+        crouchInput = Input.GetButton("Crouch");
+
+        Crouch();   
+    }
+
+    private void Crouch()
+    {
+        if (crouchInput && IsGrounded)
+        {
+            Crouching = true;
+        } 
+        else 
+        {
+            Crouching = false;
+        }
+
+        anim.SetBool("crouching", Crouching);
     }
 
     private void Jump()
