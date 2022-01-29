561df099ba4b2037e42af9c659745e517ad79774
PhillipGameDev <267723+PhillipGameDev@users.noreply.github.com>
2022-01-29 16:39:09 -0300

    coisas

--- Assets/Scripts/Sign.cs
+++ Assets/Scripts/Sign.cs
@@ -13,7 +13,6 @@ public class Sign : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             canva.SetActive(true);
-            Debug.Log("Sign active");
         }
     }
 
@@ -22,7 +21,6 @@ public class Sign : MonoBehaviour
         if(other.CompareTag("Player"))
         {
             canva.SetActive(false);
-            Debug.Log("Sign desative");
         }
     }
 }
