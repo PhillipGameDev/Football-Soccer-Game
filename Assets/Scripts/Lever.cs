5b1c7bdd46cfef8a433f77ea4b3cbd4d3f37a05e
PhillipGameDev <267723+PhillipGameDev@users.noreply.github.com>
2022-01-30 12:42:02 -0300

    toten

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -12,10 +12,12 @@ public class Lever : MonoBehaviour
     private bool isInCooldown = false;
 
     [SerializeField] private GameObject fireObject;
+    [SerializeField] private AudioSource audioFire;
 
     void Start()
     {
         fireObject.SetActive(toggleValue);
+        audioFire.mute = true;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -36,6 +38,9 @@ public class Lever : MonoBehaviour
                 if (other.CompareTag("Player"))
                 {
                     toggleValue = !toggleValue;
+                        
+                    audioFire.mute = !toggleValue;
+                    SoundManager.Instance.Play(toggleValue ? SoundManager.Instance.audioTorchOn : SoundManager.Instance.audioTorchOff);
                     fireObject.SetActive(toggleValue);
                     action.Invoke(toggleValue);
                     Debug.Log("Toggle  " + toggleValue);
